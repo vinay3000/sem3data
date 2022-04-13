e2f86dac573d9bdf8f35789db9340de70bbda82c
vinay3000 <103555449+vinay3000@users.noreply.github.com>
2022-04-13 10:42:20 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# sem3data
+Table of all the students indicating who all has A.T.K.t.
---
+++ logic.js
@@ -0,0 +1,361 @@
+let fail = "A.T.K.T"
+var one = 'https://msbte.org.in/BS19TEDIS/MBTESOMS19ESDIS/EnrollmentNumber/17/';
+var two = 'Marksheet.html';
+let computer = [
+	RUSHIKESH = {
+
+		seatno : 1715080059,
+		name : "RUSHIKESH PATIL",
+		atkt : 6,
+		perc : 'fail'
+		
+	},
+	RAJ = {
+
+		seatno : 1715080060,
+		name : "RAJ KOTHARI",
+		atkt : "NONE",
+		perc : "73.87%"
+		
+	},
+	KAUSHIK  = {
+
+		seatno : 1715080061,
+		name : "KAUSHIK MHAPSEKAR ",
+		atkt : "NONE",
+		perc : '63.13%'
+		
+	},
+	ADITI  = {
+
+		seatno : 1715080062,
+		name : "ADITI PATIL  ",
+		atkt : "NONE",
+		perc : "84.27%"
+		
+	},
+	OMKAR   = {
+
+		seatno : 1715080064,
+		name : "OMKAR  PAREKH   ",
+		atkt : "NONE",
+		perc : "80.53%"
+		
+	},
+	ARUN   = {
+
+		seatno : 1715080065,
+		name : "ARUN   YADAV    ",
+		atkt : "6",
+		perc : 'fail'
+		
+	},
+	HARSH   = {
+
+		seatno : 1715080066,
+		name : "HARSH GHARAT  ",
+		atkt : "4",
+		perc : "fail"
+		
+	},
+	ASHLEY    = {
+
+		seatno : 1715080072,
+		name : "ASHLEY POW",
+		atkt : "NONE",
+		perc : "84.93%"
+		
+	},
+	AKHIL = {
+
+		seatno : 1715080074,
+		name : "AKHIL NASKAR ",
+		atkt : "8",
+		perc : 'fail'
+		
+	},
+	HARSHAY = {
+
+		seatno : 1715080076,
+		name : "HARSHAY FUTANE ",
+		atkt : "9",
+		perc : 'fail'
+		
+	},
+	ADARSH = {
+
+		seatno : 1715080077,
+		name : "ADARSH SENGHANI ",
+		atkt : "NONE",
+		perc : "75.60%"
+		
+	},
+	SHUBHAM = {
+
+		seatno : 1715080078,
+		name : "SHUBHAM BIRJE",
+		atkt : "NONE",
+		perc : "74.00%"
+		
+	},
+	YASHAR = {
+
+		seatno : 1715080079,
+		name : "YASHAR SHAIKH",
+		atkt : "NONE",
+		perc : '71.47%'
+		
+	},
+	VAISHNAVI = {
+
+		seatno : 1715080080,
+		name : "VAISHNAVI BAISANE",
+		atkt : "NONE",
+		perc : "79.73%"
+		
+	},
+	RUCHI  = {
+
+		seatno : 1715080081,
+		name : "RUCHI TARE",
+		atkt : "NONE",
+		perc : "77.73%"
+		
+	},
+	AARTI  = {
+
+		seatno : 1715080082,
+		name : "AARTI SHINDE",
+		atkt : "NONE",
+		perc : "65.33%"
+		
+	},
+	RUGVED  = {
+
+		seatno : 1715080083,
+		name : "RUGVED PATIL",
+		atkt : "5",
+		perc : 'fail'
+		
+	},
+	ABHISHEK  = {
+
+		seatno : 1715080084,
+		name : "ABHISHEK MUGE",
+		atkt : "NONE",
+		perc : "84.13%"
+		
+	},
+	KIRTI  = {
+
+		seatno : 1715080085,
+		name : "KIRTI PATIL ",
+		atkt : "NONE",
+		perc : "75.60%"
+		
+	},
+	SEJAL  = {
+
+		seatno : 1715080086,
+		name : "SEJAL BORKAR",
+		atkt : "NONE",
+		perc : "82.80%"
+		
+	},
+	VAISHNAVI = {
+
+		seatno : 1715080087,
+		name : "VAISHNAVI BHOIR ",
+		atkt : "4",
+		perc : 'fail'
+		
+	},
+	SUYOG = {
+
+		seatno : 1715080090,
+		name : "SUYOG DHADWE ",
+		atkt : "NONE",
+		perc : "76.40%"
+		
+	},
+	SIDDHESH = {
+
+		seatno : 1715080092,
+		name : "SIDDHESH KONDASKAR",
+		atkt : "NONE",
+		perc : "83.73%"
+		
+	},
+	NEHA = {
+
+		seatno : 1715080093,
+		name : "NEHA SHENDE",
+		atkt : "NONE",
+		perc : "65.60%"
+		
+	},
+	JEET = {
+
+		seatno : 1715080097,
+		name : "JEET VAZE",
+		atkt : "NONE",
+		perc : "63.47%"
+		
+	},
+	VIJAYA = {
+
+		seatno : 1715080104,
+		name : "VIJAYA KHAIRNAR",
+		atkt : "6",
+		perc : 'fail'
+		
+	},
+	VEDANT = {
+
+		seatno : 1715080105,
+		name : "VEDANT MER",
+		atkt : "8",
+		perc : 'fail'
+		
+	},
+	JUILEE = {
+
+		seatno : 1715080108,
+		name : "JUILEE KINI",
+		atkt : "NONE",
+		perc : "74.27%"
+		
+	},
+	VINAY = {
+
+		seatno : 1715080109,
+		name : "VINAY MAURYA",
+		atkt : "2",
+		perc : "Fail"
+		
+	},
+	BEEMBAI = {
+
+		seatno : 1715080112,
+		name : "BEEMBAI KAVALDAR",
+		atkt : "8",
+		perc : 'fail'
+		
+	},
+	PRANITA = {
+
+		seatno : 1715080113,
+		name : "PRANITA AWARI",
+		atkt : "NONE",
+		perc : "70.53%"
+		
+	},
+	PRINSU = {
+
+		seatno : 1715080116,
+		name : "PRINSU TIWARI",
+		atkt : "NONE",
+		perc : "74.13%"
+		
+	},
+	ABHISHEK = {
+
+		seatno : 1715080118,
+		name : "ABHISHEK BHANDARI",
+		atkt : "NONE",
+		perc : "67.60%"
+		
+	},
+	YASH = {
+
+		seatno : 1715080119,
+		name : "YASH PUROHIT",
+		atkt : "NONE",
+		perc : "76.27%"
+		
+	},
+	ANUJ = {
+
+		seatno : 1715080120,
+		name : "ANUJ GAWDE",
+		atkt : "5",
+		perc : 'fail'
+		
+	},
+	HITESH = {
+
+		seatno : 1715080123,
+		name : "HITESH CHOUDHARY",
+		atkt : "3",
+		perc : 'fail'
+		
+	},
+	KAMLESH = {
+
+		seatno : 1715080124,
+		name : "KAMLESH PRAJAPAT",
+		atkt : "NONE",
+		perc : "78.67%"
+		
+	},
+	SHIVAM = {
+
+		seatno : 1715080399,
+		name : "SHIVAM PANDAY",
+		atkt : "None",
+		perc : '71.20%'
+		
+	},
+	
+];
+var cname; 
+function mainFunction(para1){
+	debugger;
+		$('.table tbody').empty();
+
+	para1.forEach(function(element){
+		
+		if(element.atkt > 4){
+
+			cname = "table-danger";
+			
+		}
+		else if(element.atkt != "NONE"){
+
+			cname = "table-warning";
+			
+		}
+		else{
+
+			cname = "table-success";
+			
+		}
+		
+		$('.table tbody').append(`
+		
+		
+				<tr class="${cname}" onClick="MyWindow=window.open('one+${element.seatno} + Marksheet.html','MyWindow',width=600,height=300); return false;">
+					<td>${element.name}</td>
+					<td>${element.atkt}</td>
+					<td>${element.perc}</td>
+				</tr>
+			
+		
+		`);
+		
+	});
+	
+}
+
+
+function sortbyno(){
+
+	let sorted = computer.sort();
+	mainFunction(sorted);
+	
+}
+$('document').ready(function(){
+
+	mainFunction(computer);
+	
+});
